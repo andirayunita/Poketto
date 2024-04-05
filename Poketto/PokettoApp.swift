421e1815b6232d87883ac0ef5dcd91b2272f09c7
Andira Yunita <andhirayp@gmail.com>
2024-04-05 16:09:01 +0700

    Initial Commit

---
+++ Poketto/PokettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokettoApp.swift
+//  Poketto
+//
+//  Created by Andira Yunita on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PokettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
